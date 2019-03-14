5a0e268a64dff61089b21c4d2c11144f1a9048b1
Dmitry Rakovets <dmitryrakovets@gmail.com>
2019-03-14 23:53:21 +0300

    feat: impl couting Love Triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,32 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+  for (var i = 0; i < preferences.length; i++) {
+    firstVertex = preferences[i];
+    secondVertex = preferences[firstVertex - 1];
+    thirdVertex = preferences[secondVertex - 1];
+    pseudoFirstElement = preferences[thirdVertex - 1]
+    
+    if (!isValidVertices()) {
+      continue;
+    }
+    
+    if (firstVertex === pseudoFirstElement) {
+      count++;
+      deleteTriangleElements(preferences, i);
+    }
+  };
+  return count;
 };
+
+function deleteTriangleElements(preferences, i) {
+  delete preferences[i];
+  delete preferences[firstVertex - 1];
+  delete preferences[secondVertex - 1];
+}
+
+function isValidVertices() {
+  return !((secondVertex === undefined || secondVertex === undefined || thirdVertex === undefined)
+    || firstVertex === secondVertex || secondVertex === thirdVertex || thirdVertex == firstVertex);
+}
